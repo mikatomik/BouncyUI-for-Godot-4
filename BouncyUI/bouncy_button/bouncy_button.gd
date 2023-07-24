c90653fdf6bf5c49b0ed31334b36051b499e5cd0
mika <mik@tom.ik>
2023-07-24 04:07:11 -0400

    Add support for independent scale up/down times

--- BouncyUI/bouncy_button/bouncy_button.gd
+++ BouncyUI/bouncy_button/bouncy_button.gd
@@ -12,10 +12,10 @@ class_name BouncyButton
 		return base_font_size
 
 ## Scaled font size in points. Used when IN focus.
-## If the scaled font size is big enough to cause your button size to adjust,
+## If scaled_font_size is big enough to cause your button size to adjust,
 ## it may cause other UI elements to be pushed around if they are in a shared container.
-## To get around this, increase the button's custom minimum size until the button size
-## no longer adjusts itself during text scaling.
+## To get around this, increase the button's custom minimum size, or decrease scaled_font_size
+## until the button size no longer adjusts itself during text scaling.
 ## This will likely need tweaked to suit your particular font.
 @export var scaled_font_size : int = 35 :
 	set(value):
@@ -26,6 +26,7 @@ class_name BouncyButton
 		return scaled_font_size
 		
 ## Preview the scaled font size value in the editor.
+## If animation_preview_enabled is true, the animation will play in editor when toggling this setting.
 ## Will be disabled at runtime.
 @export var view_scaled_font_size : bool = false :
 	set(value):
@@ -44,11 +45,14 @@ class_name BouncyButton
 	get:
 		return view_scaled_font_size
 
-##Speed at which the font will scale up in seconds.
-@export var scale_speed : float = 0.1
+##Amount of time the text will take to scale up in seconds.
+@export var scale_up_time : float = 0.5
+
+##Amount of time the text will take to scale down in seconds.
+@export var scale_down_time : float = 0.1
 
 ##Curve to use during scaling up of the text.
-@export var scale_up_curve_type : TRANSITION_TYPE = TRANSITION_TYPE.LINEAR
+@export var scale_up_curve_type : TRANSITION_TYPE = TRANSITION_TYPE.BOUNCE
 
 ##Curve to use during scaling down of the text.
 @export var scale_down_curve_type : TRANSITION_TYPE = TRANSITION_TYPE.LINEAR
@@ -56,7 +60,8 @@ class_name BouncyButton
 enum TRANSITION_TYPE {
 	LINEAR,##Scale using Tween.TRANS_LINEAR.
 	BOUNCE,##Scale using Tween.TRANS_BOUNCE. If the Base Font Size and Scaled Font Size are too close, this won't be perceivable. Additionally, if the Scale Speed is too low it may also be unperceivable.
-	SPRING ##Scale using Tween.TRANS_SPRING. If the Base Font Size and Scaled Font Size are too close, this won't be perceivable. Additionally, if the Scale Speed is too low it may also be unperceivable.
+	SPRING, ##Scale using Tween.TRANS_SPRING. If the Base Font Size and Scaled Font Size are too close, this won't be perceivable. Additionally, if the Scale Speed is too low it may also be unperceivable.
+	ELASTIC ## Scale using Twee.TRANS_ELASTIC.
 }
 
 ##Ease type to be used when scaling up.
@@ -73,14 +78,20 @@ enum EASE_TYPE {
 	EASE_OUT##Scale using TWEEN.EASE_OUT.
 }
 
+##Button will grab focus and animation will happen when mouse
+##is hovered over the button.
+@export var grab_focus_on_mouse_enter : bool = true
+
 ##Release focus of the button when the mouse leaves it without clicking.
-##Looks nice for PC specific platforms, but since no button will have focus
+##Looks nice for desktop specific platforms, but since no button will have focus
 ##after this is triggered, it will strand controller and keyboard users without
 ##a way to access the UI nodes unless you manually call grab_focus() on another node.
 @export var release_focus_on_mouse_exit : bool = false
 
 ##If enabled, preview tween in editor when "View Scaled Font Size" is toggled.
-@export var animation_preview_enabled : bool = false
+@export var animation_preview_enabled : bool = true
+
+var active_tween : Tween = null #Track the active tween so we can kill it before starting a new one
 
 func _enter_tree() -> void:
 	if !focus_entered.is_connected(_on_gained_focus):
@@ -98,6 +109,9 @@ func _enter_tree() -> void:
 	
 	if view_scaled_font_size:
 		view_scaled_font_size = false
+		
+	if animation_preview_enabled:
+		animation_preview_enabled = false
 
 func _on_gained_focus() -> void:
 	tween_font_size_up()
@@ -106,37 +120,46 @@ func _on_lost_focus() -> void:
 	tween_font_size_down()
 	
 func tween_font_size_down() -> void:
+	if active_tween:
+		active_tween.kill()
 	var tw = get_tree().create_tween()
 	
 	match scale_down_curve_type:
 		TRANSITION_TYPE.LINEAR: tw.set_trans(Tween.TRANS_LINEAR)
 		TRANSITION_TYPE.BOUNCE: tw.set_trans(Tween.TRANS_BOUNCE)
 		TRANSITION_TYPE.SPRING: tw.set_trans(Tween.TRANS_SPRING)
+		TRANSITION_TYPE.ELASTIC: tw.set_trans(Tween.TRANS_ELASTIC)
 	match scale_down_ease_type:
 		EASE_TYPE.EASE_IN_OUT: tw.set_ease(Tween.EASE_IN_OUT)
 		EASE_TYPE.EASE_OUT_IN: tw.set_ease(Tween.EASE_OUT_IN)
 		EASE_TYPE.EASE_IN: tw.set_ease(Tween.EASE_IN)
 		EASE_TYPE.EASE_OUT: tw.set_ease(Tween.EASE_OUT)
 		
-	tw.tween_property(self, "theme_override_font_sizes/font_size", base_font_size, scale_speed)
+	tw.tween_property(self, "theme_override_font_sizes/font_size", base_font_size, scale_down_time)
+	active_tween = tw
 
 func tween_font_size_up() -> void:
+	if active_tween:
+		active_tween.kill()
 	var tw = get_tree().create_tween()
 	
 	match scale_up_curve_type:
-		TRANSITION_TYPE.LINEAR: tw.set_trans(Tween.TRANS_LINEAR)
-		TRANSITION_TYPE.BOUNCE: tw.set_trans(Tween.TRANS_BOUNCE)
-		TRANSITION_TYPE.SPRING: tw.set_trans(Tween.TRANS_SPRING)
+		TRANSITION_TYPE.LINEAR:  tw.set_trans(Tween.TRANS_LINEAR)
+		TRANSITION_TYPE.BOUNCE:  tw.set_trans(Tween.TRANS_BOUNCE)
+		TRANSITION_TYPE.SPRING:  tw.set_trans(Tween.TRANS_SPRING)
+		TRANSITION_TYPE.ELASTIC: tw.set_trans(Tween.TRANS_ELASTIC)
 	match scale_up_ease_type:
 		EASE_TYPE.EASE_IN_OUT: tw.set_ease(Tween.EASE_IN_OUT)
 		EASE_TYPE.EASE_OUT_IN: tw.set_ease(Tween.EASE_OUT_IN)
-		EASE_TYPE.EASE_IN: tw.set_ease(Tween.EASE_IN)
-		EASE_TYPE.EASE_OUT: tw.set_ease(Tween.EASE_OUT)
+		EASE_TYPE.EASE_IN:     tw.set_ease(Tween.EASE_IN)
+		EASE_TYPE.EASE_OUT:    tw.set_ease(Tween.EASE_OUT)
 		
-	tw.tween_property(self, "theme_override_font_sizes/font_size", scaled_font_size, scale_speed)
+	tw.tween_property(self, "theme_override_font_sizes/font_size", scaled_font_size, scale_up_time)
+	active_tween = tw
 
 func _on_mouse_entered() -> void:
-	self.grab_focus()
+	if grab_focus_on_mouse_enter:
+		self.grab_focus()
 
 func _on_mouse_exited() -> void:
 	if release_focus_on_mouse_exit:
